cf168c9a6b7845998ed9897143a618115e76f01c
serge <serge.klim@outlook.com>
2025-10-09 23:05:05 +0100

    avx 128

--- test/test_simd.cpp
+++ test/test_simd.cpp
@@ -12,6 +12,22 @@ BOOST_AUTO_TEST_SUITE(utils_simd_test_suite)
 
 using test_types = boost::mpl::list<std::uint8_t, std::uint16_t, std::uint32_t, std::uint64_t> ;
 
+BOOST_AUTO_TEST_CASE_TEMPLATE(smid_128_find_test, T, test_types) {
+   constexpr auto vector_bytes_size = 128 / 8;
+   auto values = std::array<T, vector_bytes_size * sizeof(std::uint8_t) / sizeof(T) - 1>{};
+   std::ranges::copy(std::views::transform(std::views::iota(0) | std::views::take(values.size()), [](auto v) {
+                        return v * 3;
+                     }),
+                     begin(values));
+   auto vector = utils::simd::vector{values};
+   BOOST_CHECK_EQUAL(vector.size(), values.size());
+   BOOST_CHECK_EQUAL(vector.capacity(), values.size() + 1);
+   for (auto i : std::views::iota(0) | std::views::take(values.size()))
+      BOOST_CHECK_EQUAL(vector.find_first(values[i]), i);
+
+   BOOST_CHECK_GT(vector.find_first(values.back() + 33), values.size());
+}
+
 BOOST_AUTO_TEST_CASE_TEMPLATE(smid_256_find_test, T, test_types) {
    constexpr auto vector_bytes_size = 256 / 8;
    auto values = std::array<T, vector_bytes_size * sizeof(std::uint8_t) / sizeof(T)  - 1>{};
