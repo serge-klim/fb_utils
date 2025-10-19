dc624901568bced36bec0e9e0cd6393488d92361
serge <serge.klim@outlook.com>
2025-10-19 07:54:38 +0100

    different arch

--- utils/simd/vector.hpp
+++ utils/simd/vector.hpp
@@ -1,193 +1,30 @@
 ﻿#pragma once
-#include "immintrin.h"
+#if defined(__i386__) || defined(__x86_64__)
+#include "x64/vector.hpp"
+#else
+#include "not_supported/vector.hpp"
+#endif 
+#include "detail.hpp"
 #include <span>
 #include <array>
 #include <cstring>
 #include <concepts>
-#include <limits>
-#include <cstddef>
+#include <algorithm>
+#include <type_traits>
 #include <cstdint>
 
 namespace utils { inline namespace v1 { namespace simd {
+         
 
-template <typename T>
-constexpr std::size_t bit_size(std::size_t size = 1) noexcept {
-   return sizeof(T) / sizeof(std::uint8_t) * 8 * size;
-}
-            
-namespace detail {
-
-template <typename T>
-constexpr std::size_t vector_size(std::size_t size = 1) noexcept {
-   auto bits = bit_size<T>(size);
-#ifdef __SSE__
-   if (bits <= 128)
-      return 128;
-#ifdef __AVX2__
-   if (bits <= 256)
-      return 256;
-#ifdef __AVX512BW__
-   if (bits <= 512)
-      return 512;
-   if (bits <= 1024)
-      return 1024;
-#endif // __AVX512BW__
-#endif // __AVX2__
-#endif // __SSE__
-     return 1024 * 10;
-}
-
-template <std::size_t Size>
-struct vector;
-
-template <>
-struct vector<128> {
-   template <typename T, std::size_t Size>
-   static auto load(std::span<T, Size> const& s) {
-      if constexpr (Size * sizeof(T) == sizeof(data))
-         return _mm_loadu_si128(reinterpret_cast<__m128i const*>(s.data()));
-      else {
-         auto tmp = std::array<std::byte, 128 / 8>{};
-         std::memcpy(tmp.data(), s.data(), Size * sizeof(T) / sizeof(std::byte));
-         return _mm_loadu_si128(reinterpret_cast<__m128i const*>(tmp.data()));
-      }
-   }
-   __m128i data;
-};
-
-template <typename T>
-   requires(std::is_integral_v<T> && sizeof(T) == sizeof(std::uint8_t))
-std::uint8_t find_first(__m128i& data, T value) noexcept {
-   auto comparand = _mm_set1_epi8(static_cast<char>(value));
-   auto const res = static_cast<unsigned int>(_mm_movemask_epi8(_mm_cmpeq_epi8(data, comparand)));
-   return res != 0 ? static_cast<std::uint8_t>(_tzcnt_u32(res) * sizeof(std::byte) / sizeof(T)) : (std::numeric_limits<std::uint8_t>::max)();
-}
-
-template <typename T>
-   requires(std::is_integral_v<T> && sizeof(T) == sizeof(std::uint16_t))
-std::uint8_t find_first(__m128i& data, T value) noexcept {
-   auto comparand = _mm_set1_epi16(static_cast<short>(value));
-   auto const res = static_cast<unsigned int>(_mm_movemask_epi8(_mm_cmpeq_epi16(data, comparand)));
-   return res != 0 ? static_cast<std::uint8_t>(_tzcnt_u32(res) * sizeof(std::byte) / sizeof(T)) : (std::numeric_limits<std::uint8_t>::max)();
-}
-
-template <typename T>
-   requires(std::is_integral_v<T> && sizeof(T) == sizeof(std::uint32_t))
-std::uint8_t find_first(__m128i& data, T value) noexcept {
-   auto comparand = _mm_set1_epi32(static_cast<int>(value));
-   auto const res = static_cast<unsigned int>(_mm_movemask_epi8(_mm_cmpeq_epi32(data, comparand)));
-   return res != 0 ? static_cast<std::uint8_t>(_tzcnt_u32(res) * sizeof(std::byte) / sizeof(T)) : (std::numeric_limits<std::uint8_t>::max)();
-}
-
-template <typename T>
-   requires(std::is_integral_v<T> && sizeof(T) == sizeof(std::uint64_t))
-std::uint8_t find_first(__m128i& data, T value) noexcept {
-   auto comparand = _mm_set1_epi64x(static_cast<long long>(value));
-   auto const res = static_cast<unsigned int>(_mm_movemask_epi8(_mm_cmpeq_epi64(data, comparand)));
-   return res != 0 ? static_cast<std::uint8_t>(_tzcnt_u32(res) * sizeof(std::byte) / sizeof(T)) : (std::numeric_limits<std::uint8_t>::max)();
-}
-
-template <>
-struct vector<256> {
-   template<typename T, std::size_t Size>
-   static auto load(std::span<T, Size> const& s) {
-      if constexpr (Size * sizeof(T) == sizeof(data))
-         return _mm256_loadu_si256(reinterpret_cast<__m256i const*>(s.data()));
-      else {
-         auto tmp = std::array<std::byte, 256 / 8>{};
-         std::memcpy(tmp.data(), s.data(), Size * sizeof(T) / sizeof(std::byte));
-         return _mm256_loadu_si256(reinterpret_cast<__m256i const*>(tmp.data()));
-      }
-   }
-   __m256i data;
-};
-
-template <typename T>
-   requires(std::is_integral_v<T> && sizeof(T) == sizeof(std::uint8_t))
-std::uint8_t find_first(__m256i& data, T value) noexcept {
-   auto comparand = _mm256_set1_epi8(static_cast<char>(value));
-   auto const res = static_cast<unsigned int>(_mm256_movemask_epi8(_mm256_cmpeq_epi8(data, comparand)));
-   return res != 0 ? static_cast<std::uint8_t>(_tzcnt_u32(res) * sizeof(std::byte) / sizeof(T)) : (std::numeric_limits<std::uint8_t>::max)();
-}
-
-template <typename T>
-requires(std::is_integral_v<T> && sizeof(T) == sizeof(std::uint16_t))
-std::uint8_t find_first(__m256i& data, T value) noexcept {
-   auto comparand = _mm256_set1_epi16(static_cast<short>(value));
-   auto const res = static_cast<unsigned int>(_mm256_movemask_epi8(_mm256_cmpeq_epi16(data, comparand)));
-   return res != 0 ? static_cast<std::uint8_t>(_tzcnt_u32(res) * sizeof(std::byte) / sizeof(T)) : (std::numeric_limits<std::uint8_t>::max)();
-}
-
-template <typename T>
-   requires(std::is_integral_v<T> && sizeof(T) == sizeof(std::uint32_t))
-std::uint8_t find_first(__m256i& data, T value) noexcept {
-   auto comparand = _mm256_set1_epi32(static_cast<int>(value));
-   auto const res = static_cast<unsigned int>(_mm256_movemask_epi8(_mm256_cmpeq_epi32(data, comparand)));
-   return res != 0 ? static_cast<std::uint8_t>(_tzcnt_u32(res) * sizeof(std::byte) / sizeof(T)) : (std::numeric_limits<std::uint8_t>::max)();
-}
-
-template <typename T>
-   requires(std::is_integral_v<T> && sizeof(T) == sizeof(std::uint64_t))
-std::uint8_t find_first(__m256i& data, T value) noexcept {
-   auto comparand = _mm256_set1_epi64x(static_cast<long long>(value));
-   auto const res = static_cast<unsigned int>(_mm256_movemask_epi8(_mm256_cmpeq_epi64(data, comparand)));
-   return res != 0 ? static_cast<std::uint8_t>(_tzcnt_u32(res) * sizeof(std::byte) / sizeof(T)) : (std::numeric_limits<std::uint8_t>::max)();
-}
-
-template <>
-struct vector<512> {
-   __m512i data;
-   template <typename T, std::size_t Size>
-   static auto load(std::span<T, Size> const& s) {
-      if constexpr (Size * sizeof(T) == sizeof(data))
-         return _mm512_loadu_si512(reinterpret_cast<__m512i const*>(s.data()));
-      else {
-         auto tmp = std::array<std::byte, 512 / 8>{};
-         std::memcpy(tmp.data(), s.data(), Size * sizeof(T) / sizeof(std::byte));
-         return _mm512_loadu_si512(reinterpret_cast<__m512i const*>(tmp.data()));
-      }
-   }
-};
-
-template <typename T>
-   requires(std::is_integral_v<T> && sizeof(T) == sizeof(std::uint8_t))
-std::uint8_t find_first(__m512i& data, T value) noexcept {
-   auto comparand = _mm512_set1_epi8(static_cast<char>(value));
-   auto res = _mm512_cmpeq_epi8_mask(data, comparand);
-   return res != 0 ? static_cast<std::uint8_t>(_tzcnt_u64(res)) : (std::numeric_limits<std::uint8_t>::max)();
-}
-
-template <typename T>
-   requires(std::is_integral_v<T> && sizeof(T) == sizeof(std::uint16_t))
-std::uint8_t find_first(__m512i& data, T value) noexcept {
-   auto comparand = _mm512_set1_epi16(static_cast<short>(value));
-   auto res = _mm512_cmpeq_epi16_mask(data, comparand);
-   return res != 0 ? static_cast<std::uint8_t>(_tzcnt_u64(res)) : (std::numeric_limits<std::uint8_t>::max)();
-}
-
-template <typename T>
-   requires(std::is_integral_v<T> && sizeof(T) == sizeof(std::uint32_t))
-std::uint8_t find_first(__m512i& data, T value) noexcept {
-   auto comparand = _mm512_set1_epi32(static_cast<short>(value));
-   auto res = _mm512_cmpeq_epi32_mask(data, comparand);
-   return res != 0 ? static_cast<std::uint8_t>(_tzcnt_u64(res)) : (std::numeric_limits<std::uint8_t>::max)();
-}
-
-template <typename T>
-   requires(std::is_integral_v<T> && sizeof(T) == sizeof(std::uint64_t))
-std::uint8_t find_first(__m512i& data, T value) noexcept {
-   auto comparand = _mm512_set1_epi64(static_cast<short>(value));
-   auto res = _mm512_cmpeq_epi64_mask(data, comparand);
-   return res != 0 ? static_cast<std::uint8_t>(_tzcnt_u64(res)) : (std::numeric_limits<std::uint8_t>::max)();
-}
-
-} // namespace detail
 
 template <std::integral T, std::size_t Size>
 class vector : detail::vector<detail::vector_size<T>(Size)> {
    using base_t = detail::vector<detail::vector_size<T>(Size)>;
  public:
    using size_type = std::uint8_t;
+
+   vector() = default;
+
    template <std::size_t N>
    requires (N <= Size)
    vector(std::span<T const, N> v) noexcept
@@ -199,10 +36,18 @@ class vector : detail::vector<detail::vector_size<T>(Size)> {
    vector(std::array<T, N> const& v) noexcept : vector(std::span<T const, N>{v.data(), N}) {}
 
    template <std::size_t N>
-   vector(T (&v)[N]) noexcept : vector(std::span<T const, N>{v,}) {}
+   vector(T(&v)[N]) noexcept : vector(std::span<T const, N>{v,}) {}
+
+   template <std::size_t N>
+   requires(N <= Size)
+   vector& operator = (std::span<T const, N> v) noexcept {
+      base_t::data = base_t::load(v);
+      size_ = static_cast<size_type>(std::span{v}.size());
+      return *this;
+   }
 
    constexpr size_type size() const noexcept { return size_; }
-   static constexpr size_type capacity() noexcept { return static_cast<size_type>(Size); }
+   static constexpr size_type capacity() noexcept { return base_t::template capacity<T>(); }
    auto find_first(T value) noexcept { return detail::find_first(base_t::data, value); }
  private:
    size_type size_ = 0;
@@ -221,5 +66,26 @@ template <typename T>
 constexpr bool can_vectorize(std::size_t size) noexcept {
    return detail::vector_size<T>(size) <= 512;
 }
+template <typename... T>
+   requires(std::is_integral_v<std::common_type_t<T...>> && can_vectorize<std::common_type_t<T...>>(sizeof...(T)))
+auto make_vector(T&&... v) {
+   using value_type = std::common_type_t<T...>;
+   constexpr auto byte_size = detail::vector_size<value_type>(sizeof...(T)) / 8;
+   auto ids = std::array<std::common_type_t<T...>, byte_size * sizeof(std::uint8_t) / sizeof(value_type)>{std::forward<T>(v)...};
+   return ::utils::simd::vector{ids};
+}
+
+template <std::size_t BitSize, typename... T>
+   requires(std::is_integral_v<std::common_type_t<T...>>)
+auto make_vector(T&&... v) {
+   using value_type = std::common_type_t<T...>;
+   constexpr auto byte_size = (std::max)(detail::vector_size<value_type>(sizeof...(T)), BitSize) / 8;
+   static_assert(can_vectorize<std::uint8_t>(byte_size),
+                 "too big to fit in avalible simd vector");
+   auto ids = std::array<std::common_type_t<T...>, byte_size * sizeof(std::uint8_t) / sizeof(value_type)>{std::forward<T>(v)...};
+   return ::utils::simd::vector{ids};
+}
+
+
             
 }}} // namespace utils::v1::simd
